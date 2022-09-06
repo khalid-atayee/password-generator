d7ad3e5cbf8020870bb34a6423f6314a8d9f2727
khalid-atayee <107976140+khalid-atayee@users.noreply.github.com>
2022-09-06 16:01:27 +0430

    password generator completed just working on functionality that make the input field empty

---
+++ script.js
@@ -0,0 +1,30 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+// console.log(characters[0]);
+let btn = document.querySelector(".btn");
+let numberOfChar = document.getElementById('demo');
+let firstPass = document.getElementById("firstPass");
+let secondPass = document.getElementById("secondPass");
+let firstPassGen='',secondPassGen,char;
+
+
+btn.addEventListener("click",()=>{
+    // firstPass.value=0;
+    numberOfChar =numberOfChar.value;
+    firstPass.textContent= genratePassword(numberOfChar);
+    secondPass.textContent= genratePassword(numberOfChar);
+    // numberOfChar="";
+    // location.reload();
+    
+});
+
+function genratePassword(numberOfChar){
+    for(let i=0; i<=numberOfChar; i++){
+        char = Math.floor(Math.random()*characters.length);
+        firstPassGen+=characters[char];
+    }
+    return firstPassGen;
+
+
+}
+
